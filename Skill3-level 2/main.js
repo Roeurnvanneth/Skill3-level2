827308c74aa3aa48521981fcd903a7cc4dff417b
Roeurnvanneth <roeurn.vanneth@institute.pse.ngo>
2025-05-02 08:35:14 +0700

    first commit

---
+++ Skill3-level 2/main.js
@@ -0,0 +1,23 @@
+let getDayName(dayNumber);{}
+switch(1-7) {
+  case 2:
+        System.out.println("Tuesday");
+        break;
+      case 3:
+        System.out.println("Wednesday");
+        break;
+      case 4:
+        System.out.println("Thursday");
+        break;
+      case 5:
+        System.out.println("Friday");
+        System.out.println("TGIF!");
+        break;
+      case 6:
+        System.out.println("Saturday");
+        break;
+      case 7:
+        System.out.println("Sunday");
+        break;  
+}
+console.log("Enter a day number (1-7)")
